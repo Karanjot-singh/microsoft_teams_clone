97879adb32a1d43fa3bd5c084a3517a2ae9b690f
karanjot-singh <karanjot19050@iiitd.ac.in>
2021-06-21 05:12:47 +0530

    Onboarding Welcome screen added

---
+++ lib/screens/onboarding/rounded_button.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import '../../constants.dart';
+
+class RoundedButton extends StatelessWidget {
+  final String buttonText;
+  final Function onPress;
+  final Color buttonColor, textColor;
+  const RoundedButton({
+    Key key,
+    this.buttonText,
+    this.onPress,
+    this.buttonColor,
+    this.textColor = Colors.white,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      width: size.width * 0.6,
+      child: TextButton(
+        onPressed: onPress,
+        child: Text(
+          buttonText,
+          style: TextStyle(
+            fontSize: 18,
+          ),
+        ),
+        style: TextButton.styleFrom(
+            primary: textColor,
+            backgroundColor: buttonColor,
+            shape: StadiumBorder()),
+      ),
+    );
+  }
+}
--- lib/screens/onboarding/welcome_screen.dart
+++ lib/screens/onboarding/welcome_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:microsoft_teams_clone/constants.dart';
+import 'rounded_button.dart';
 
 class WelcomeScreen extends StatelessWidget {
   @override
@@ -15,19 +17,38 @@ class Body extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     // This size makes the app responsive to various screen sizes
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Text(
-          "Welcome to Microsoft Teams!",
-          style: TextStyle(fontWeight: FontWeight.bold),
-        ),
-        Text(
-          "App developed by Karanjot Singh",
-          style: TextStyle(fontWeight: FontWeight.bold),
-        ),
-        SvgPicture.asset("assets/")
-      ],
+    return SafeArea(
+      minimum: const EdgeInsets.all(20),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            "Welcome to Microsoft Teams!",
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 30,
+              color: appPrimaryColor,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(height: 20),
+          Text(
+            "App developed by Karanjot Singh",
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 20),
+          SvgPicture.asset(
+            "assets/icons/onboarding_chat.svg",
+            height: size.height * 0.45,
+          ),
+          SizedBox(height: 20),
+          RoundedButton(
+            buttonColor: appPrimaryColor,
+            buttonText: "Next",
+            onPress: () {},
+          ),
+        ],
+      ),
     );
   }
 }
