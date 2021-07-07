c9b0a63f6c12b76320e606cdb0f378304862a0ba
karanjot-singh <karanjot19050@iiitd.ac.in>
2021-07-07 15:32:57 +0530

    sign in screen code modified

--- lib/pages/onboarding/authentication.dart
+++ lib/pages/onboarding/authentication.dart
@@ -1,8 +1,9 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'user_info_screen.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-import 'package:microsoft_teams_clone/pages/onboarding/user_info_screen.dart';
 
 class Authentication {
   static Future<FirebaseApp> initializeFirebase({
@@ -21,6 +22,7 @@ class Authentication {
         ),
       );
     }
+
     return firebaseApp;
   }
 
@@ -32,6 +34,23 @@ class Authentication {
         style: TextStyle(color: Colors.redAccent, letterSpacing: 0.5),
       ),
     );
+  }
+
+  static Future<void> signOut({required BuildContext context}) async {
+    final GoogleSignIn googleSignIn = GoogleSignIn();
+
+    try {
+      if (!kIsWeb) {
+        await googleSignIn.signOut();
+      }
+      await FirebaseAuth.instance.signOut();
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        Authentication.customSnackBar(
+          content: 'Error signing out. Try again.',
+        ),
+      );
+    }
   }
 
   static Future<User?> signInWithGoogle({required BuildContext context}) async {
@@ -81,19 +100,5 @@ class Authentication {
     }
 
     return user;
-  }
-
-  static Future<void> signOut({required BuildContext context}) async {
-    final GoogleSignIn googleSignIn = GoogleSignIn();
-
-    try {
-      await googleSignIn.signOut();
-    } catch (e) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        Authentication.customSnackBar(
-          content: 'Error signing out. Try again.',
-        ),
-      );
-    }
   }
 }
---
+++ lib/pages/onboarding/google_sign_in_button.dart
@@ -0,0 +1,60 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:microsoft_teams_clone/config/constants.dart';
+import 'authentication.dart';
+import 'user_info_screen.dart';
+
+class GoogleSignInButton extends StatefulWidget {
+  @override
+  _GoogleSignInButtonState createState() => _GoogleSignInButtonState();
+}
+
+class _GoogleSignInButtonState extends State<GoogleSignInButton> {
+  bool _isSigningIn = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 16.0),
+      child: _isSigningIn
+          ? CircularProgressIndicator(
+              valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+            )
+          : OutlinedButton.icon(
+              style: OutlinedButton.styleFrom(
+                primary: Colors.black,
+                shape: StadiumBorder(),
+                padding: EdgeInsets.all(20),
+              ),
+              onPressed: () async {
+                setState(() {
+                  _isSigningIn = true;
+                });
+
+                User? user =
+                    await Authentication.signInWithGoogle(context: context);
+
+                setState(() {
+                  _isSigningIn = false;
+                });
+
+                if (user != null) {
+                  Navigator.of(context).pushReplacement(
+                    MaterialPageRoute(
+                      builder: (context) => UserInfoScreen(
+                        user: user,
+                      ),
+                    ),
+                  );
+                }
+              },
+              icon: FaIcon(FontAwesomeIcons.google, color: appPurpleColor),
+              label: Text(
+                'Sign In With Google',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+              ),
+            ),
+    );
+  }
+}
