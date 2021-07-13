217246efda6157083012d8720a7962852771c2f1
karanjot-singh <karanjot19050@iiitd.ac.in>
2021-07-13 05:54:31 +0530

    Onboarding Screens added

--- lib/pages/onboarding/video_screen.dart
+++ lib/pages/onboarding/video_screen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:microsoft_teams_clone/config/constants.dart';
+import 'package:microsoft_teams_clone/pages/login/sign_in_page.dart';
 import 'package:microsoft_teams_clone/pages/onboarding/welcome_screen.dart';
 import 'package:page_transition/page_transition.dart';
+import 'package:stream_chat_flutter/stream_chat_flutter.dart';
 import '../../widgets/rounded_button.dart';
 import 'share_screen.dart';
 
@@ -27,58 +29,86 @@ class VideoBody extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     // This size makes the app responsive to various screen sizes
-    return SafeArea(
-      minimum: const EdgeInsets.all(20),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            "Connect with your loved ones via conference calls",
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 30,
-              color: Colors.black,
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: EdgeInsets.only(
+          left: 20,
+          right: 20,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              "Connect with friends via conference calls",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 30,
+                color: Colors.black,
+              ),
+              textAlign: TextAlign.center,
             ),
-            textAlign: TextAlign.center,
-          ),
-          SizedBox(height: size.height * 0.03),
-          // SvgPicture.asset(
-          //   "assets/icons/onboarding_video.svg",
-          //   height: size.height * 0.35,
-          // ),
-          SizedBox(height: size.height * 0.03),
-          RoundedButton(
-            buttonColor: appPurpleColor,
-            textColor: Colors.white,
-            buttonText: "Next",
-            onPress: () {
-              // Navigates to the next screen
-              Navigator.push(
-                context,
-                PageTransition(
-                  type: PageTransitionType.rightToLeftWithFade,
-                  child: ShareScreen(),
+            SizedBox(height: size.height * 0.03),
+            SvgPicture.asset(
+              "assets/icons/onboarding_video.svg",
+              height: size.height * 0.40,
+            ),
+            SizedBox(height: size.height * 0.01),
+            Container(
+              height: size.height * 0.055,
+              width: size.width * 0.5,
+              child: TextButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    PageTransition(
+                      type: PageTransitionType.rightToLeftWithFade,
+                      child: SignInPage(),
+                    ),
+                  );
+                },
+                child: Text(
+                  "Next",
+                  style: TextStyle(
+                    fontSize: 18,
+                  ),
                 ),
-              );
-            },
-          ),
-          SizedBox(height: 10),
-          RoundedButton(
-            buttonColor: appAccentColor,
-            textColor: Colors.black,
-            buttonText: "Previous",
-            onPress: () {
-              // Navigates to the next screen
-              Navigator.push(
-                context,
-                PageTransition(
-                  type: PageTransitionType.leftToRightWithFade,
-                  child: WelcomeScreen(),
+                style: TextButton.styleFrom(
+                    primary: Colors.white,
+                    backgroundColor: appPurpleColor,
+                    shape: StadiumBorder()),
+              ),
+            ),
+            SizedBox(height: size.height * 0.03),
+            Container(
+              height: size.height * 0.055,
+              width: size.width * 0.5,
+              child: TextButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    PageTransition(
+                      type: PageTransitionType.leftToRightWithFade,
+                      child: WelcomeScreen(),
+                    ),
+                  );
+                },
+                child: Text(
+                  "Previous",
+                  style: TextStyle(
+                    fontSize: 18,
+                  ),
                 ),
-              );
-            },
-          ),
-        ],
+                style: TextButton.styleFrom(
+                    primary: Colors.white,
+                    backgroundColor: appPurpleColor,
+                    shape: StadiumBorder()),
+              ),
+            ),
+            SizedBox(height: size.height * 0.03),
+          ],
+        ),
       ),
     );
   }
--- lib/pages/onboarding/welcome_screen.dart
+++ lib/pages/onboarding/welcome_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:microsoft_teams_clone/config/constants.dart';
+import 'package:microsoft_teams_clone/pages/login/sign_in_page.dart';
+import 'package:microsoft_teams_clone/pages/onboarding/share_screen.dart';
 import 'package:microsoft_teams_clone/pages/onboarding/video_screen.dart';
+import 'package:microsoft_teams_clone/routes/routes.dart';
 import 'package:page_transition/page_transition.dart';
 import '../../widgets/rounded_button.dart';
 
@@ -19,46 +22,65 @@ class Body extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     // This size makes the app responsive to various screen sizes
-    return SafeArea(
-      minimum: const EdgeInsets.all(20),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            "Welcome to Microsoft Teams!",
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 30,
-              color: appPurpleColor,
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: EdgeInsets.only(
+          left: 20,
+          right: 20,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              "Welcome to Microsoft Teams!",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 30,
+                color: appPurpleColor,
+              ),
+              textAlign: TextAlign.center,
             ),
-            textAlign: TextAlign.center,
-          ),
-          // SizedBox(height: size.height * 0.03),
-          // Text(
-          //   "App developed by Karanjot Singh",
-          //   style: TextStyle(fontWeight: FontWeight.bold),
-          // ),
-          SizedBox(height: size.height * 0.03),
-          // SvgPicture.asset(
-          //   "assets/icons/onboarding_chat.svg",
-          //   height: size.height * 0.40,
-          // ),
-          SizedBox(height: size.height * 0.03),
-          RoundedButton(
-            buttonColor: appPurpleColor,
-            buttonText: "Next",
-            onPress: () {
-              // Navigates to the next screen
-              Navigator.push(
-                context,
-                PageTransition(
-                  type: PageTransitionType.rightToLeftWithFade,
-                  child: VideoScreen(),
+            // SizedBox(height: size.height * 0.03),
+            // Text(
+            //   "App developed by Karanjot Singh",
+            //   style: TextStyle(fontWeight: FontWeight.bold),
+            // ),
+            SizedBox(height: size.height * 0.03),
+            SvgPicture.asset(
+              "assets/icons/onboarding_chat.svg",
+              height: size.height * 0.40,
+            ),
+            SizedBox(height: size.height * 0.03),
+            Container(
+              height: size.height * 0.055,
+              width: size.width * 0.5,
+              child: TextButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    PageTransition(
+                      type: PageTransitionType.rightToLeftWithFade,
+                      child: ShareScreen(),
+                    ),
+                  );
+                },
+                child: Text(
+                  "Proceed",
+                  style: TextStyle(
+                    fontSize: 18,
+                  ),
                 ),
-              );
-            },
-          ),
-        ],
+                style: TextButton.styleFrom(
+                    primary: Colors.white,
+                    backgroundColor: appPurpleColor,
+                    shape: StadiumBorder()),
+              ),
+            ),
+            SizedBox(height: size.height * 0.03),
+          ],
+        ),
       ),
     );
   }
